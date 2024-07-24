13d5ab7c96d09cf3e9280696626c5e9ae275afef
mune <munerato.ale@gmail.com>
2024-07-24 14:24:47 +0200

    Implemented main activity

--- app/src/main/java/com/example/demowasmandroid/MainActivity.kt
+++ app/src/main/java/com/example/demowasmandroid/MainActivity.kt
@@ -1,46 +1,51 @@
 package com.example.demowasmandroid
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.demowasmandroid.ui.theme.DemoWasmAndroidTheme
+import org.extism.chicory.sdk.Manifest
+import org.extism.chicory.sdk.Plugin
+import java.nio.charset.StandardCharsets
 
 class MainActivity : ComponentActivity() {
+    private val TAG = "MainActivity"
+
+    private fun runGreetings(wasmData: ByteArray, name: String) : String {
+        Log.d(TAG, "Start runGreetings with name '$name'")
+        val manifest: Manifest = Manifest.fromBytes(wasmData)
+        val plugin = Plugin(manifest, arrayOf(), LoggerBridge("ChicoryRuntime"))
+        val result = String(
+            plugin.call("greet", name.toByteArray(StandardCharsets.UTF_8)),
+            StandardCharsets.UTF_8
+        )
+        Log.d(TAG, "Result from runGreetings: $result")
+        return result
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DemoWasmAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.main_activity)
+        val nameEditText: EditText = findViewById(R.id.nameEditText)
+        val btn: Button = findViewById(R.id.runButton)
+        val textView: TextView = findViewById(R.id.greetingsTextView)
+
+        btn.setOnClickListener {
+            val wasmFile = assets.open("greet.wasm")
+            val size: Int = wasmFile.available()
+            val buffer = ByteArray(size)
+            wasmFile.read(buffer)
+            wasmFile.close()
+
+            val greetings = runGreetings(buffer, nameEditText.text.toString())
+            textView.text = greetings
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DemoWasmAndroidTheme {
-        Greeting("Android")
     }
 }
